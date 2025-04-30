827e642b20a206dcbdf062f54bba1b3dec5f20bd
Mficius <mayisficius@gmail.com>
2025-04-30 12:45:39 +0100

    new test sonarqube

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,34 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 
+import math  # inutilisé dans le fichier
+
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+def unused_function():
+    print("Je ne suis jamais appelé !")
+
+def do_something():
+    temp = 42  # jamais utilisé
+    return "OK"
+
+def greet_user():
+    print("Hello, user!")
+
+def greet_admin():
+    print("Hello, user!")  # duplication
+
+def complex_function(x):
+    if x > 0:
+        if x < 10:
+            if x % 2 == 0:
+                if x % 3 == 0:
+                    return "Divisible by 6"
+    return "Other"
+
+
+def divide(a, b):
+    return a / b  # peut échouer si b == 0
