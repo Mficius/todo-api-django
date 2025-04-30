24ca44378d97d99751ef732504bce2e2f56ee6ed
Mficius <mayisficius@gmail.com>
2025-04-30 01:29:00 +0100

    new changes

--- todo/settings.py
+++ todo/settings.py
@@ -12,10 +12,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY=env("SECRET_KEY")
 
 DEBUG = True
 
